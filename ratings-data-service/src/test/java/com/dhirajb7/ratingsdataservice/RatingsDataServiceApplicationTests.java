2e9157f3054db7f11ed26e2ef3b5a96a49366c1a
fernando0922 <fernando.vidal.uw@gmail.com>
2023-05-07 23:48:02 +0530

    barebone so far, updated gitignore

---
+++ ratings-data-service/src/test/java/com/dhirajb7/ratingsdataservice/RatingsDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhirajb7.ratingsdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
